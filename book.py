53969ab4abddc0feb027fa4219de68ebf058855b
kongminhyuk <80611251+kongminhyuk@users.noreply.github.com>
2021-05-21 03:42:35 +0900

    Create book.py

---
+++ book.py
@@ -0,0 +1,16 @@
+class book:  #클래스 book 정의
+    def select_menu(self):
+        system.SelectMenu()
+    def add_book(self):
+        system.addbook()
+    def find_book(self):
+        system.findbook()
+    def modify_book(self):
+        system.modifybook()    
+    def remove_book(self):
+        system.removebook()
+    def view_all(self):
+        system.viewall()
+
+
+
