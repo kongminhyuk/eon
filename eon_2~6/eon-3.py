7f9c33cca404217473fc7a32c8a8ff1306b88e32
kongminhyuk <80611251+kongminhyuk@users.noreply.github.com>
2021-05-21 05:20:25 +0900

    Create eon-5.py

---
+++ eon_2~6/eon-3.py
@@ -0,0 +1,10 @@
+a = list(map(int,input("합 할 숫자를 입력하시오: ").split()))
+#입력받은 수를 리스트 a에 저장
+
+def sumlist(a): #sumlist 함수 선언
+    result = 0 #result 의 초기값 설정
+    for n in a: #for 반복문 사용
+        result = result + n
+    return result #반복문이 끝난 후 결과 값 반환
+    
+print("합은 %d입니다." %sumlist(a)) #결과 값 출력
---
+++ eon_2~6/eon-5.py
@@ -0,0 +1,8 @@
+a = list(map(int,input("array를 입력해주세요: ").split())) #입력하는 수를 받아 배열로 저장(array)
+b = list(map(int,input("i, j, k: ").split())) #i,j,k를 입력받아 배열로 저장(commands)
+
+def ARRAY(x): #array 함수 선언
+    x = x[b[0]-1:b[1]] # 0번째 부터 존재하므로 -1
+    x.sort() # x를 정렬
+    return x[b[2]-1] #b[K-1]로 return
+print(ARRAY(a)) #array(x)에 인수 a를 넣고 출력
